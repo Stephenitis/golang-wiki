72ce0b7f7adb828b0051c66272d1484dfe8b9a7c
Stephen Nguyen <nguyenhstephen@gmail.com>
2014-02-07 14:18:22 -0800

    detail about the ocatal integer for write and read permissions

--- wiki.go
+++ wiki.go
@@ -4,3 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+	// The octal integer literal 0600, passed as the third parameter to WriteFile, indicates that the file should be created with read-write permissions for the current user only. (See the Unix man page open(2) for details.)
+}
